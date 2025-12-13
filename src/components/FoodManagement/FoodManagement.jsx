949ecebe6533aae57f777fe045f28194126540a3
dagger983 <kmvignesh1406@gmail.com>
2025-12-13 14:42:04 +0530

    first commit

--- src/components/FoodManagement/FoodManagement.jsx
+++ src/components/FoodManagement/FoodManagement.jsx
@@ -22,7 +22,7 @@ const FoodManagement = () => {
     "Biryani", "Pizza", "Burger", "Fried Chicken", "Mutton", "Chicken", 
     "Sea Foods", "South Indian", "Dosa", "Parotta", "Fried Rice", 
     "Naan & Gravy", "Noodles", "Veg", "Rolls", "Soup", "Tea", "Coffee", 
-    "Shakes", "Mojito", "Cake's", "Ice Cream", "Fresh Juice"
+    "Shakes", "Mojito", "Cake's", "Ice Cream", "Fresh Juice","Crispy","Sandwich","Sweets","Momos"
   ];
 
   // Time slots
